6beca97ad3c1eac12885d9fa12e10f53ac530c02
yyueniao <91043129+yyueniao@users.noreply.github.com>
2022-07-03 08:43:31 +0800

    Update and rename bot.py to main.py

--- boardgame_bot/main.py
+++ boardgame_bot/main.py
@@ -1,35 +1,22 @@
-import discord
 from discord.ext import commands
 import json
 import os
-import re
-from cmds.main import Main
 
 with open("setting.json",'r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
 
-bot = commands.Bot(command_prefix='-')
+bot = commands.Bot(command_prefix='*')
 players = []
 
 @bot.event
 async def on_ready():
     print(">>机器人已到场<<")
 
-@bot.command()
-async def load(ctx, extension):
-    bot.load_extension(f"cmds.{extension}")
-    ctx.send(f"{extension} 加载完成")
-
-@bot.command()
-async def unload(ctx, extension):
-    bot.unload_extension(f"cmds.{extension}")
-    ctx.send(f"{extension} 卸载完成")
-
+    
 @bot.command()
 async def reload(ctx, extension):
-    bot.reload_extension(f"cmds.{extension}")
-    ctx.send(f"{extension} 更新完成")
-
+    bot.reload_extension(f"{extension}")
+    await ctx.send(f"{extension} 更新完成")
 
 
 for filename in os.listdir('./cmds'):
